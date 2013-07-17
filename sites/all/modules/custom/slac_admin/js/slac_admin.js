469687f4f01569ee6b4b7acb46b009e15fa2d947
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2013-07-17 11:40:41 +0300

    Task #6856: Disable lightbox on small resolution fixes.

--- sites/all/modules/custom/slac_admin/js/slac_admin.js
+++ sites/all/modules/custom/slac_admin/js/slac_admin.js
@@ -1,15 +1,15 @@
 (function ($) {
   Drupal.behaviors.displayResolutionSettings = {
     attach: function (context, settings) {
-      var width = Drupal.settings.slac_admin.width;
-      var height = Drupal.settings.slac_admin.height;
 
-      if (width == 0 || height == 0) {
-      	return;
-      }
-
-      if (width >= screen.width || height >= screen.height) {
-      	$('a[rel^="lightbox"]').find('img').unwrap();
+      if (typeof Drupal.settings.slac_admin != 'undefined'
+        && typeof Drupal.settings.slac_admin.width != 'undefined'
+        && typeof Drupal.settings.slac_admin.height != 'undefined') {
+        var width = Drupal.settings.slac_admin.width;
+        var height = Drupal.settings.slac_admin.height;
+        if (width >= screen.width || height >= screen.height) {
+          $('a[rel^="lightbox"]').find('img').unwrap();
+        }
       }
     }
   }
