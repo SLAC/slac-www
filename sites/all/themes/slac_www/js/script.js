13aafb3b3e7316aa030a3d122aea663f75d1646a
Kirill Cebotari PC <kirill.cebotari@wearepropeople.com>
2013-09-13 10:32:46 +0300

    Issue: Placeholder js selector changed to search

--- sites/all/themes/slac_www/js/script.js
+++ sites/all/themes/slac_www/js/script.js
@@ -173,7 +173,7 @@
 
   Drupal.behaviors.placeholderIE = {
     attach: function (context, settings) {
-      $("[placeholder]").textPlaceholder();
+      $("#search-box .form-text").textPlaceholder();
     }
   }
 
