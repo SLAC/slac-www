bdb8d7f49f704058a5f90ad2aa266ee16da9c4ab
Alexandru Vrabie <propeople@220v.(none)>
2013-09-17 15:30:50 +0300

    Task #7729: I have modified class to links-internal-external-icons

--- sites/all/modules/custom/slac_int_ext_links/js/slac_int_ext_links.js
+++ sites/all/modules/custom/slac_int_ext_links/js/slac_int_ext_links.js
@@ -4,13 +4,12 @@
         attach: function(context) {
             internal_links = Drupal.settings.slac_int_ext_links.internal_links;
             external_links = Drupal.settings.slac_int_ext_links.external_links;
-            $('.int-ext-links a').each(function(index, element){
-                //
-                console.log($(this));
-                console.log(element);
-                console.log(internal_links.indexOf(element.href));
+            $('.links-internal-external-icons a').each(function(index, element){
                 if(internal_links.indexOf(element.href) > -1){
                     $(this).addClass('internal');
+                }
+                if(external_links.indexOf(element.href) > -1){
+                    $(this).addClass('external');
                 }
             });
         }
