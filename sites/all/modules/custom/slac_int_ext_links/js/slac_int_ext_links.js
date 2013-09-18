1263f28f09e78be315f53edec33d02ecc7a6c0d2
Alexandru Vrabie <propeople@220v.(none)>
2013-09-18 16:23:40 +0300

    Task #7729: I have fixed js. Now css class added properly

--- sites/all/modules/custom/slac_int_ext_links/js/slac_int_ext_links.js
+++ sites/all/modules/custom/slac_int_ext_links/js/slac_int_ext_links.js
@@ -1,24 +1,24 @@
 (function ($) {
 
-  Drupal.behaviors.slacIntExtLinks = {
-    attach: function (context) {
-      var internal_links = Drupal.settings.slac_int_ext_links.internal_links.split("\n");
-      var external_links = Drupal.settings.slac_int_ext_links.external_links.split("\n");
-      all_links = $.merge($.merge([], internal_links), external_links);
-      $('.links-internal-external-icons a').each(function (index, element) {
-        for (var i = 0; i < all_links.length; i++) {
-          patt = new RegExp(all_links[i]);
-          if (patt.test(element.href) == true) {
-            if ($.inArray(all_links[i], internal_links)){
-              $(this).addClass('internal');
-            }
-            else if($.inArray(all_links[i], external_links) ){
-              $(this).addClass('external');
-            }
-          }
+    Drupal.behaviors.slacIntExtLinks = {
+        attach: function (context) {
+            var internal_links = Drupal.settings.slac_int_ext_links.internal_links;
+            var external_links = Drupal.settings.slac_int_ext_links.external_links;
+            $('.links-internal-external-icons a').each(function (index, element) {
+                link_checker(internal_links, $(this), 'internal')
+                link_checker(external_links, $(this), 'external')
+            });
         }
-      });
     }
-  }
-
+    // check if is valid regex and add class
+    function link_checker(links_array, obj, css_class){
+        var href = obj.attr("href");
+        for (var i = 0; i < links_array.length; i++) {
+            var link = $.trim(links_array[i]);
+            patt = new RegExp(link);
+            if (patt.test(href) == true) {
+                obj.addClass(css_class);
+            }
+        }
+    }
 })(jQuery);
