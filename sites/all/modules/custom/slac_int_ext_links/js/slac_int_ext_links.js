1428d1f1d8443a1c95c919c332711b4572c61cc1
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2013-09-18 02:42:53 -0700

    Issue #7729: Fix the js for regular expressions.

--- sites/all/modules/custom/slac_int_ext_links/js/slac_int_ext_links.js
+++ sites/all/modules/custom/slac_int_ext_links/js/slac_int_ext_links.js
@@ -1,23 +1,24 @@
 (function ($) {
 
-    Drupal.behaviors.slacIntExtLinks = {
-        attach: function(context) {
-            internal_links = Drupal.settings.slac_int_ext_links.internal_links;
-            external_links = Drupal.settings.slac_int_ext_links.external_links;
-            all_links = $.merge($.merge([], internal_links), external_links);
-            $('.links-internal-external-icons a').each(function(index, element){
-                for (var i = 0; i < all_links.length; i++) {
-                    patt = new RegExp(all_links[i]);
-                    if(patt.test(element.href) == true){
-                        /*if(jQuery.inArray(all_links[i], internal_links)){
-                            $(this).addClass('internal');
-                        }else if(jQuery.inArray(all_links[i], external_links) ){
-                            $(this).addClass('external');
-                        }*/
-                    }
-                }
-            });
+  Drupal.behaviors.slacIntExtLinks = {
+    attach: function (context) {
+      var internal_links = Drupal.settings.slac_int_ext_links.internal_links.split("\n");
+      var external_links = Drupal.settings.slac_int_ext_links.external_links.split("\n");
+      all_links = $.merge($.merge([], internal_links), external_links);
+      $('.links-internal-external-icons a').each(function (index, element) {
+        for (var i = 0; i < all_links.length; i++) {
+          patt = new RegExp(all_links[i]);
+          if (patt.test(element.href) == true) {
+            if ($.inArray(all_links[i], internal_links)){
+              $(this).addClass('internal');
+            }
+            else if($.inArray(all_links[i], external_links) ){
+              $(this).addClass('external');
+            }
+          }
         }
+      });
     }
+  }
 
 })(jQuery);
