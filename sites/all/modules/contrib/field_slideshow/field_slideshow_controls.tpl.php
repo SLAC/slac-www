d0894663d0c45373b1fe687be65232620dada8c9
Elena Creacea <elena.cracea@wearepropeople.com>
2013-09-26 14:38:50 +0300

    slider buttons change

--- sites/all/modules/contrib/field_slideshow/field_slideshow_controls.tpl.php
+++ sites/all/modules/contrib/field_slideshow/field_slideshow_controls.tpl.php
@@ -7,10 +7,10 @@
  */
 ?>
 <div id="field-slideshow-<?php print $slideshow_id; ?>-controls" class="field-slideshow-controls">
-  <a href="#" class="prev"><?php print t('Prev'); ?></a>
+  <a href="#" class="prev"><span class="prev-button"><?php print t('Prev'); ?></span></a>
   <?php if (!empty($controls_pause)) : ?>
     <a href="#" class="play"><?php print t('Play'); ?></a>
     <a href="#" class="pause"><?php print t('Pause'); ?></a>
   <?php endif; ?>
-  <a href="#" class="next"><?php print t('Next'); ?></a>
+  <a href="#" class="next"><span class="prev-button"><?php print t('Next'); ?></span></a>
 </div>
