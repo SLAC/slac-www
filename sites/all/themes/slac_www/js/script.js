624054d00add0eab76622f3695c4fc60e65c556c
Elena Creacea <elena.cracea@wearepropeople.com>
2013-09-26 15:03:31 +0300

    slider

--- sites/all/themes/slac_www/js/script.js
+++ sites/all/themes/slac_www/js/script.js
@@ -161,6 +161,10 @@
       // on slider btns click
       $('div[class*="views-slideshow-controls"]').find('a,.views-content-counter').click(resizer);
     }
+
+    /* Controls */
+
+    $( ".prev, .next" ).wrapInner( "<span></span>");
   }
 
   Drupal.behaviors.externalLinks = {
