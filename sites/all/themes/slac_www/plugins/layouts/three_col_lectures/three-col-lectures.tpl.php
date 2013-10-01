e9a53f6e49b80fade009d0e47815974e4eb01512
Elena Creacea <elena.cracea@wearepropeople.com>
2013-10-01 18:47:29 +0300

    lectures layout changes

--- sites/all/themes/slac_www/plugins/layouts/three_col_lectures/three-col-lectures.tpl.php
+++ sites/all/themes/slac_www/plugins/layouts/three_col_lectures/three-col-lectures.tpl.php
@@ -17,41 +17,41 @@
 <div class="panel-display three-col clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="three-col-middle-right">
     <div class="panel-panel three-col-middle">
-      <div class="lectures-gray-content">
-        <div class="panel-panel three-col-middle-top">
-          <?php if ($content['middle-top']): ?>
-            <div class="inside"><?php print $content['middle-top']; ?></div>
-          <?php endif ?>
-        </div>
-      <div class="panel-panel three-col-middle-top-seccond">
-        <?php if ($content['middle-top-seccond']): ?>
-          <div class="inside"><?php print $content['middle-top-seccond']; ?></div>
+      <div class="panel-panel three-col-middle-top">
+        <?php if ($content['middle-top']): ?>
+          <div class="inside"><?php print $content['middle-top']; ?></div>
         <?php endif ?>
       </div>
-        <div class="panel-panel three-col-middle-center">
-          <div class="panel-panel three-col-middle-center-left">
-            <?php if ($content['middle-center-left']): ?>
-              <div class="inside"><?php print $content['middle-center-left']; ?></div>
-            <?php endif ?>
-          </div>
-          <div class="panel-panel three-col-middle-center-right">
-            <?php if ($content['middle-center-right']): ?>
-              <div class="inside"><?php print $content['middle-center-right']; ?></div>
-            <?php endif ?>
-          </div>
+      <div class="lectures-gray-content">
+        <div class="panel-panel three-col-middle-top-seccond">
+          <?php if ($content['middle-top-seccond']): ?>
+            <div class="inside"><?php print $content['middle-top-seccond']; ?></div>
+          <?php endif ?>
         </div>
-        <div class="panel-panel three-col-middle-bottom">
-          <div class="panel-panel three-col-middle-bottom-left">
-            <?php if ($content['middle-bottom-left']): ?>
-              <div class="inside"><?php print $content['middle-bottom-left']; ?></div>
-            <?php endif ?>
+          <div class="panel-panel three-col-middle-center">
+            <div class="panel-panel three-col-middle-center-left">
+              <?php if ($content['middle-center-left']): ?>
+                <div class="inside"><?php print $content['middle-center-left']; ?></div>
+              <?php endif ?>
+            </div>
+            <div class="panel-panel three-col-middle-center-right">
+              <?php if ($content['middle-center-right']): ?>
+                <div class="inside"><?php print $content['middle-center-right']; ?></div>
+              <?php endif ?>
+            </div>
           </div>
-          <div class="panel-panel three-col-middle-bottom-right">
-            <?php if ($content['middle-bottom-right']): ?>
-              <div class="inside"><?php print $content['middle-bottom-right']; ?></div>
-            <?php endif ?>
+          <div class="panel-panel three-col-middle-bottom">
+            <div class="panel-panel three-col-middle-bottom-left">
+              <?php if ($content['middle-bottom-left']): ?>
+                <div class="inside"><?php print $content['middle-bottom-left']; ?></div>
+              <?php endif ?>
+            </div>
+            <div class="panel-panel three-col-middle-bottom-right">
+              <?php if ($content['middle-bottom-right']): ?>
+                <div class="inside"><?php print $content['middle-bottom-right']; ?></div>
+              <?php endif ?>
+            </div>
           </div>
-        </div>
       </div>
     </div>
     <div class="panel-panel three-col-right">
