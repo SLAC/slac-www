87bc689c9f579fcfed733f4d839b1b9364569012
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2013-10-04 18:07:55 +0300

    Task #8077: First commit.

---
+++ sites/all/modules/custom/pp_bi/plugins/block_insert/lang/en.js
@@ -0,0 +1,8 @@
+CKEDITOR.plugins.setLang('pp_bi', 'en', {
+    pp_bi_button: 'Insert a block'
+    /*atom_cut: 'Cut atom',
+    atom_paste: 'Paste atom',
+    atom_delete: 'Delete atom from textarea',
+    atom_none: 'Please select an atom first',
+    link_image_only: 'This option is currently available for Image Atoms only.'*/
+});
