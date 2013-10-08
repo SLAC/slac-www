7820e627b92d6db68dcae5b3fb0216ead9276a0e
Andrei Jechiu <andrei.jechiu@wearepropeople.com>
2013-10-08 17:37:42 +0300

    Task #8077: Select block type.

--- sites/all/modules/custom/pp_bi/plugins/block_insert/block_insert.js
+++ sites/all/modules/custom/pp_bi/plugins/block_insert/block_insert.js
@@ -1,14 +1,23 @@
 (function($) {
+
+  Drupal.CTools = Drupal.CTools || {};
+  Drupal.CTools.Modal = Drupal.CTools.Modal || {};
+  Drupal.block_insert = Drupal.block_insert || {};
+
   //Define the WYSIWYG plugin.
   Drupal.wysiwyg.plugins.block_insert = {
     invoke: function (data, settings, instanceId) {
       if (data.format == 'html') {
-        //alert(Drupal.t('This button does nothing now.'));
-          /*Drupal.CTools.Modal.show()
-          $('#modal-title').html(Drupal.t('Title'));
-          $('#modal-content').html('content here');*/
-          $.get(Drupal.settings.basePath + 'block/add/pp-bi/nojs/1');
+          $.get(
+            Drupal.settings.basePath + 'block/add/pp-bi/nojs',
+            function(data) {
+              Drupal.CTools.Modal.show()
+              $('#modal-title').html(Drupal.t('Add a new block'));
+              $('#modal-content').html(data);
+            }
+          );
       }
+      //Drupal.wysiwyg.instances[instanceId].insert('content');
     }
   };
 })(jQuery);
