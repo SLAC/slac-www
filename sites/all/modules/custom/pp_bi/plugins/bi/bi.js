173e12b864a3861fc561c81dd1b26f3bd5e8da93
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2013-10-09 14:58:37 -0700

    Issue #8077: Enabling/disabling wysiwyg replacing blocks with tokens.

--- sites/all/modules/custom/pp_bi/plugins/bi/bi.js
+++ sites/all/modules/custom/pp_bi/plugins/bi/bi.js
@@ -21,6 +21,58 @@
           insert.prompt(settings.global);
         }
       }
+    },
+
+    /**
+   * Attach function, called when a rich text editor loads.
+   * This finds all [[tags]] and replaces them with the html
+   * that needs to show in the editor.
+   *
+   * This finds all JSON macros and replaces them with the HTML placeholder
+   * that will show in the editor.
+   */
+    attach: function (content, settings, instanceId) {
+      bi_ensure_tagmap();
+
+      var bi_tagmap = Drupal.settings.bi_tagmap,
+      matches = content.match(/\[block_insert\]\d*\[\/block_insert\]/g);
+
+      if (matches) {
+        for (var index in matches) {
+          var macro = matches[index];
+
+          if (bi_tagmap[macro]) {
+            content = content.replace(macro, bi_tagmap[macro]);
+          }
+          else {
+            debug.debug("Could not find content for " + macro);
+          }
+        }
+      }
+      return content;
+    },
+
+    /**
+    * Detach function, called when a rich text editor detaches.
+    */
+    detach: function (content, settings, instanceId) {
+      bi_ensure_tagmap();
+      var bi_tagmap = Drupal.settings.bi_tagmap,
+      i = 0,
+      markup,
+      macro;
+
+      var matches = content.match(/<div class="block-insert(.)*<!-- block-insert --><\/div>/gi);
+      if (matches) {
+        for (i = 0; i < matches.length; i++) {
+          markup = matches[i];
+          macro = bi_create_macro(markup);
+          bi_tagmap[macro] = markup;
+          content = content.replace(markup, macro);
+        }
+      }
+
+      return content;
     }
   };
 
@@ -34,19 +86,33 @@
       Drupal.bi.popups.blockSelectDialog($.proxy(this, 'insert'), settings);
     },
 
-   /**
+    /**
    * Insert HTML to the WYSIWYG when block has been created. Set the macro.
    */
     insert: function (block) {
-      var markup = '<div class="block-insert" data-block_insert="' + block.bid + '">' + block.html + '</div>',
-            macro = '{{' + block.bid + '}}';
+      var markup = '<div class="block-insert" data-block_insert="' + block.bid + '">' + block.html + '<!-- block-insert --></div>',
+      macro = bi_create_macro(markup);
 
       // Insert placeholder markup into wysiwyg.
       Drupal.wysiwyg.instances[this.instanceId].insert(markup);
-      // Store macro/markup pair in the tagmap.
-      Drupal.settings.tagmap = Drupal.settings.tagmap || {};
-      Drupal.settings.tagmap[macro] = markup;
+      // Store macro/markup pair in the bi_tagmap.
+      bi_ensure_tagmap();
+      Drupal.settings.bi_tagmap[macro] = markup;
     }
   };
+
+  /**
+   * Ensure that block insert tagmap initialized.
+   */
+  function bi_ensure_tagmap () {
+    Drupal.settings.bi_tagmap = Drupal.settings.bi_tagmap || {};
+  }
+
+  /**
+   * Create a macro token from the block.
+   */
+  function bi_create_macro(html) {
+    return '[block_insert]' + $(html).data('block_insert') + '[/block_insert]';
+  }
 
 })(jQuery);
