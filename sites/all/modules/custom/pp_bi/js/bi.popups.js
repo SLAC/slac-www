7864340f284a959c9c330ad0e62dcbe07e690572
Yuriy Gerasimov <yuri.gerasimov@gmail.com>
2013-10-11 15:19:02 -0700

    Issue #8077: Blocks can be edited.

--- sites/all/modules/custom/pp_bi/js/bi.popups.js
+++ sites/all/modules/custom/pp_bi/js/bi.popups.js
@@ -81,6 +81,7 @@
       },
       widget: { // Settings for the actual iFrame which is launched.
         src: Drupal.settings.bi.browserUrl, // Src of the media browser (if you want to totally override it)
+        updateSrc: Drupal.settings.bi.browserUpdateBlockUrl, // Src of the page to edit the block
         onLoad: Drupal.bi.popups.blockSelectDialog.onLoad // Onload function when iFrame loads.
       },
       dialog: Drupal.media.popups.getDialogOptions()
@@ -110,6 +111,69 @@ Drupal.bi.popups.blockSelectDialog.onLoad = function() {
 
   $('#blockInsertBrowser').contents().find('a.fake-cancel').bind('click', Drupal.bi.popups.blockSelectDialog.submit);
 }
+
+Drupal.bi.popups.blockUpdateDialog = function (update, bean_bid, globalOptions, pluginOptions, widgetOptions) {
+
+    var options = Drupal.bi.popups.blockSelectDialog.getDefaults();
+    options.global = $.extend({}, options.global, globalOptions);
+    options.plugins = pluginOptions;
+    options.widget = $.extend({}, options.widget, widgetOptions);
+
+    // Create it as a modal window.
+    var browserSrc = options.widget.updateSrc + bean_bid;
+//    if ($.isArray(browserSrc) && browserSrc.length) {
+//      browserSrc = browserSrc[browserSrc.length - 1];
+//    }
+//    // Params to send along to the iframe.  WIP.
+//    var params = {};
+//    $.extend(params, options.global);
+//    params.plugins = options.plugins;
+//
+//    browserSrc += '&' + $.param(params);
+    var mediaIframe = Drupal.bi.popups.getPopupIframe(browserSrc, 'blockInsertBrowser');
+    Drupal.bi.popups.blockSelectDialog.mediaframe = mediaIframe;
+    // Attach the onLoad event
+    mediaIframe.bind('load', options, options.widget.onLoad);
+    /**
+     * Setting up the modal dialog
+     */
+
+    var ok = 'OK';
+    var cancel = 'Cancel';
+    var notSelected = 'You have not selected anything!';
+
+    if (Drupal && Drupal.t) {
+      ok = Drupal.t(ok);
+      cancel = Drupal.t(cancel);
+      notSelected = Drupal.t(notSelected);
+    }
+
+    // @todo: let some options come through here. Currently can't be changed.
+    var dialogOptions = options.dialog;
+
+    dialogOptions.buttons[ok] = function () {
+      var selected = Drupal.bi.popups.blockSelectDialog.selectedBlock;
+      if (selected.length < 1) {
+        alert(notSelected);
+        return;
+      }
+      update(selected);
+      $(this).dialog("destroy");
+      $(this).remove();
+    };
+
+    dialogOptions.buttons[cancel] = function () {
+      $(this).dialog("destroy");
+      $(this).remove();
+    };
+
+    Drupal.media.popups.setDialogPadding(mediaIframe.dialog(dialogOptions));
+    // Remove the title bar.
+    mediaIframe.parents(".ui-dialog").find(".ui-dialog-titlebar").remove();
+    Drupal.media.popups.overlayDisplace(mediaIframe.parents(".ui-dialog"));
+
+    return mediaIframe;
+  };
 
 })(jQuery);
 
--- sites/all/modules/custom/pp_bi/plugins/bi/bi.js
+++ sites/all/modules/custom/pp_bi/plugins/bi/bi.js
@@ -1,8 +1,61 @@
 (function($) {
+  Drupal.behaviors.biSettings = {
+    attach: function(context, settings) {
+      CKEDITOR.config.allowedContent = true;
+      CKEDITOR.config.extraPlugins = 'widget';
+    }
+  }
+
+  Drupal.behaviors.bi = {
+    attach: function(context, settings) {
+
+      $('iframe').each(function(){
+        $(this).load(function(){
+          $(this).contents().find('.block-insert').each(function() {
+            var block = this;
+
+            $(this).parents('html').once('block-insert').click(function(){
+              $(block).removeClass('block-insert-active');
+              updateBlockInsertButtonState();
+            });
+
+            $(this).once('block-insert').click(function(event){
+              event.stopPropagation();
+
+              $(block).addClass('block-insert-active');
+              updateBlockInsertButtonState();
+            });
+          });
+        });
+      });
+
+      function updateBlockInsertButtonState() {
+        $('iframe').each(function(){
+          var insertBlockIsActive = $(this).contents().find('.block-insert-active').length;
+
+          if (insertBlockIsActive) {
+            $(this).parents('.cke_inner').find('.cke_button__bi').addClass('cke_button_on').removeClass('cke_button_off');
+          }
+          else {
+            $(this).parents('.cke_inner').find('.cke_button__bi').addClass('cke_button_off').removeClass('cke_button_on');
+          }
+        });
+      };
+
+      $('iframe.cke_wysiwyg_frame').contents().find('html').click(function(){
+          $(this).find('.cke_button__bi').addClass('cke_button_off').removeClass('cke_button_on');
+      });
+
+    }
+  };
+
   Drupal.wysiwyg.plugins.bi = {
 
-    isNode: function(node) {
-      return $(node).is('img.media-element');
+    /**
+     * Get the active block from the iframe.
+     */
+    getActiveBlockInsert: function(instanceId) {
+      return $('#cke_' + instanceId).find('iframe').contents().find('.block-insert-active');
     },
 
     /**
@@ -11,13 +64,15 @@
     invoke: function (data, settings, instanceId) {
       if (data.format == 'html') {
         var insert = new InsertBlock(instanceId);
-        if (this.isNode(data.node)) {
-          // Change the view mode for already-inserted media.
-          var media_file = extract_file_info($(data.node));
-          insert.onSelect([media_file]);
+        var $activeBlock = this.getActiveBlockInsert(instanceId);
+
+        if ($activeBlock.length > 0) {
+          var bean_bid = $activeBlock.data('block_insert');
+          // Update existing block.
+          insert.onSelect(bean_bid, settings.global);
         }
         else {
-          // Insert new media.
+          // Insert new block.
           insert.prompt(settings.global);
         }
       }
@@ -49,6 +104,8 @@
           }
         }
       }
+
+      Drupal.attachBehaviors();
       return content;
     },
 
@@ -82,19 +139,47 @@
   };
 
   InsertBlock.prototype = {
+    /**
+     * Display popup to insert new block.
+     */
     prompt: function (settings) {
       Drupal.bi.popups.blockSelectDialog($.proxy(this, 'insert'), settings);
     },
+
+  /**
+   * On selection of a media item, display item's display configuration form.
+   */
+  onSelect: function (bean_bid, settings) {
+    Drupal.bi.popups.blockUpdateDialog($.proxy(this, 'update'), bean_bid, settings);
+  },
 
     /**
    * Insert HTML to the WYSIWYG when block has been created. Set the macro.
    */
     insert: function (block) {
-      var markup = '<div class="block-insert" data-block_insert="' + block.bid + '">' + block.html + '<!-- block-insert --></div>',
+      var markup = bi_create_markup(block);
       macro = bi_create_macro(markup);
 
       // Insert placeholder markup into wysiwyg.
       Drupal.wysiwyg.instances[this.instanceId].insert(markup);
+      Drupal.attachBehaviors();
+
+      // Store macro/markup pair in the bi_tagmap.
+      bi_ensure_tagmap();
+      Drupal.settings.bi_tagmap[macro] = markup;
+    },
+
+    /**
+     * Update already inserted block.
+     */
+    update: function (block) {
+      var markup = bi_create_markup(block);
+      var macro = bi_create_macro(markup);
+
+      // Replace block old markup with new one.
+      $('#cke_' + this.instanceId).find('iframe').contents().find('.block-insert-active').html($(markup).html()).removeClass('block-insert-active');
+      updateBlockInsertButtonState();
+
       // Store macro/markup pair in the bi_tagmap.
       bi_ensure_tagmap();
       Drupal.settings.bi_tagmap[macro] = markup;
@@ -113,6 +198,13 @@
    */
   function bi_create_macro(html) {
     return '[block_insert]' + $(html).data('block_insert') + '[/block_insert]';
+  }
+
+  /**
+   * Create a markup from block object.
+   */
+  function bi_create_markup(block) {
+    return '<div class="block-insert" data-block_insert="' + block.bid + '" contenteditable="false">' + block.html + '<!-- block-insert --></div>';
   }
 
 })(jQuery);
