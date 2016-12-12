a4d39377c4931dbe7b3ddfd7b849a77faae43897
Vincent Flesouras <x.algorithm@gmail.com>
2016-12-12 12:42:33 -0800

    Eliminating youtube issue, panel fixes, webform security issues

--- sites/all/modules/contrib/tablesorter/tablesortervar.js
+++ sites/all/modules/contrib/tablesorter/tablesortervar.js
@@ -6,7 +6,27 @@
 (function ($) {
   Drupal.behaviors.tablesorter = {
     attach: function (context, settings) {
-      $(".tablesorter").tablesorter();
+
+      var widgets = [];
+      var widgetsZebra = [];
+
+      if (settings.tablesorter.zebra == 1) {
+          widgets.push('zebra');
+          widgetsZebra.push(settings.tablesorter.odd);
+          widgetsZebra.push(settings.tablesorter.even);
+      }
+    $('.tablesorter').once('tablesorter', function() {
+      $(".tablesorter").tablesorter({
+          widgets: widgets,
+          widgetsZebra: {css: widgetsZebra}
+      })
+      if ($("#tablesorter_pager").length != 0) {
+          $(".tablesorter").tablesorterPager({
+              container: $("#tablesorter_pager")
+          });
+      }
+    });
+
     }
   };
 })(jQuery);
