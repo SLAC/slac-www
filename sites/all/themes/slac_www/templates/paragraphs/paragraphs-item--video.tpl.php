44010c41b79926eed0512a95f9d682a560f11030
Kerry Murphy <kerry@xenomedia.com>
2019-09-04 07:59:33 -0500

    adds paragraphs module

---
+++ sites/all/themes/slac_www/templates/paragraphs/paragraphs-item--video.tpl.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Paragraphs item simple template.
+ */
+?>
+<?php
+  hide($content);
+
+  $width_field = '';
+  if (!empty($content['field_width'])) {
+    $width_field = ' ' . trim(strip_tags(render($content['field_width'])));
+  }
+?>
+<div class="paragraph-item paragraph-type--video<?php print $width_field; ?>">
+  <div>
+    <div>
+      <div><?php print render($content['field_pvideo']); ?></div>
+    </div>
+  </div>
+</div>
---
+++ sites/all/themes/slac_www/templates/paragraphs/paragraphs-items.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @file
+ * Paragraphs items template.
+ */
+?>
+<?php print $content; ?>
