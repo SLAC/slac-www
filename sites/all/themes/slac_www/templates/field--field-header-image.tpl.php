0f90b59ce39eccc32c7aac2afb7d9837fcdec9a8
Senem Hartung <senem@xenomedia.com>
2019-10-11 14:20:27 -0500

    adding caption when header image is not limited

--- sites/all/themes/slac_www/templates/field--field-header-image.tpl.php
+++ sites/all/themes/slac_www/templates/field--field-header-image.tpl.php
@@ -56,7 +56,9 @@ HTML comment.
 <?php if ($element['#object']->field_do_not_display_hero_image['und']['0']['value'] != 1): ?>
 
 	<?php if ($element['#object']->field_do_not_limit_header_image_['und']['0']['value'] != 1): ?>
-		<div class="header-image-as-background" style="background-image:url('<?php print $image_url; ?>');"></div>
+		<div class="header-image-as-background" style="background-image:url('<?php print $image_url; ?>');">
+      <div class="header-image-bg-caption"><?php print $items['0']['field_description']['#items']['0']['value'] ?></div>  
+    </div>
 	<?php else: ?>
 		<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
   			<?php if (!$label_hidden): ?>
