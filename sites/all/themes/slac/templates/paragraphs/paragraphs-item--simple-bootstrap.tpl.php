02c724a88933a7302749db93a75b9f975238793c
Senem Hartung <senem@xenomedia.com>
2020-07-13 14:20:57 -0500

    fixing animation class

--- sites/all/themes/slac/templates/paragraphs/paragraphs-item--simple-bootstrap.tpl.php
+++ sites/all/themes/slac/templates/paragraphs/paragraphs-item--simple-bootstrap.tpl.php
@@ -25,10 +25,10 @@ if (!empty($content['field_padding_bottom'])) {
     $padding_bottom = trim(strip_tags(render($content['field_padding_bottom'])));
 }
 if (!empty($content['field_animation'])) {
-    $animation = trim(strip_tags(render($content['field_animation'])));
+    $animation = 'wow animated ' . trim(strip_tags(render($content['field_animation'])));
 }
 if (!empty($content['field_animation_setting'])) {
-    $animation_setting = 'wow animated ' . trim(strip_tags(render($content['field_animation_setting'])));
+    $animation_setting = trim(strip_tags(render($content['field_animation_setting'])));
 }
 
 $classes = 'paragraph paragraph--type--simple-bootstrap paragraph--view-mode--default ' . $background . ' ' . $width;
