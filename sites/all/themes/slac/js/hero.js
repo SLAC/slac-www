2ed1077d9c65de53da90a5a15e4eacdcea14488c
Senem Hartung <senem@xenomedia.com>
2020-07-13 16:18:19 -0500

    handling xeno hero javascript

--- sites/all/themes/slac/js/hero.js
+++ sites/all/themes/slac/js/hero.js
@@ -1,20 +1,22 @@
 (function ($, Drupal) {
 
-"use strict";
+  $(document).ready(function(){
 
   // Makes the parallax function.
   function parallaxIt() {
 
     // Create variables.
     var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    alert('in parallax');
 
     // Adds on window scroll event.
-    //$(window).on('scroll resize', function() {
-      //scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    //});
+    $(window).live('scroll resize', function() {
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    });
 
     // For each of background parallax elements.
     $('.paragraph--type--xeno-hero .paragraph--type--xeno-hero__image').each(function(){
+      alert('in each');
       var $backgroundObj = $(this);
       var yPos;
       var coords;
@@ -22,7 +24,8 @@
 
       $backgroundObj.css('background-image', 'url(' + $backgroundObj.find('img').attr('src') + ')');
 
-      $(window).on('scroll resize', function() {
+      $(window).live('scroll resize', function() {
+        alert('somewhere');
         var docViewTop = $(window).scrollTop();
         var docViewBottom = docViewTop + $(window).height();
         var offset = $backgroundObj.parent().attr('data-offset');
@@ -47,5 +50,6 @@
   };
 
   parallaxIt();
+  });
 
 })(jQuery, Drupal);
