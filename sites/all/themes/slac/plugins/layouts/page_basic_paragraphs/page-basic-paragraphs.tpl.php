c68137a85a01d7eeb50052c65e94f4f6aa9c44ae
Kerry Murphy <kerry@xenomedia.com>
2020-07-17 12:18:03 -0500

    updates styles for menu

--- sites/all/themes/slac/plugins/layouts/page_basic_paragraphs/page-basic-paragraphs.tpl.php
+++ sites/all/themes/slac/plugins/layouts/page_basic_paragraphs/page-basic-paragraphs.tpl.php
@@ -21,47 +21,49 @@
       <?php print $content['menu']; ?>
     <?php endif ?>
   </div>
-  <div class="header-section">
-    <div class="container">
-       <?php if ($content['header']): ?>
-         <?php print $content['header']; ?>
-        <?php endif ?>
-      <div class="row">
-        <div class="col-md-12">
-          <?php if ($content['mainnav']): ?>
-            <?php print $content['mainnav']; ?>
-          <?php endif ?>
-        </div>
-      </div>
-    </div>
-  </div>
-    <?php if ($content['secondnav']): ?>
-    <div class="secondary-nav">
+  <div class="page-content">
+    <div class="header-section">
       <div class="container">
-         <div class="row">
-          <div class="site-icon">
-            <?php print $content['icon']; ?>
-          </div>
-          <div class="nav-secondary">
-            <?php print $content['secondnav']; ?>
-          </div>
-           <?php if ($content['cta']): ?>
-            <div class="cta"><?php print $content['cta']; ?></div>
+         <?php if ($content['header']): ?>
+           <?php print $content['header']; ?>
+          <?php endif ?>
+        <div class="row">
+          <div class="col-md-12">
+            <?php if ($content['mainnav']): ?>
+              <?php print $content['mainnav']; ?>
             <?php endif ?>
+          </div>
         </div>
       </div>
     </div>
-    <?php endif ?>
+      <?php if ($content['secondnav']): ?>
+      <div class="secondary-nav">
+        <div class="container">
+           <div class="row">
+            <div class="site-icon">
+              <?php print $content['icon']; ?>
+            </div>
+            <div class="nav-secondary">
+              <?php print $content['secondnav']; ?>
+            </div>
+             <?php if ($content['cta']): ?>
+              <div class="cta"><?php print $content['cta']; ?></div>
+              <?php endif ?>
+          </div>
+        </div>
+      </div>
+      <?php endif ?>
 
 
-   <?php if ($content['content']): ?>
-      <?php print $content['content']; ?>
-    <?php endif ?>
+     <?php if ($content['content']): ?>
+        <?php print $content['content']; ?>
+      <?php endif ?>
 
-  <div class="footer-section">
-  <div class="panel-panel footer">
-     <?php if ($content['footer']): ?>
-      <?php print $content['footer']; ?>
-    <?php endif ?>
+    <div class="footer-section">
+    <div class="panel-panel footer">
+       <?php if ($content['footer']): ?>
+        <?php print $content['footer']; ?>
+      <?php endif ?>
+    </div>
   </div>
 </div>
