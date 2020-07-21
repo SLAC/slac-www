7f55dfe9151ed885eb90a89aee7f8e41a44f9566
Senem Hartung <senem@xenomedia.com>
2020-07-21 10:04:21 -0500

    adding bg image to timeline bundler

--- sites/all/themes/slac/templates/paragraphs/paragraphs-item--timeline-bundler.tpl.php
+++ sites/all/themes/slac/templates/paragraphs/paragraphs-item--timeline-bundler.tpl.php
@@ -4,9 +4,10 @@
  * @file
  * Paragraphs item template.
  */
+$bg_image = render($content['field_background_image']);
 ?>
 <div class="time-line-container">
-  <ul class="time-line">
+  <ul class="time-line" style="background-image:url('<?php print $bg_image; ?>');">
     <?php print render($content); ?>
   </ul>
 </div>
