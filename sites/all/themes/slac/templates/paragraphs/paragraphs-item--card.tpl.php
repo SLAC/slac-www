48aed75a34e444285b76654a1d363951b7716932
Kerry Murphy <kerry@xenomedia.com>
2020-08-18 16:01:03 -0500

    fixes timeline on mobile

--- sites/all/themes/slac/templates/paragraphs/paragraphs-item--card.tpl.php
+++ sites/all/themes/slac/templates/paragraphs/paragraphs-item--card.tpl.php
@@ -37,7 +37,7 @@ if (!empty($content['field_paragraph_link'])) {
         <div class="card-header"><?php if ($link) : ?><a href="<?php print $link; ?>"><?php endif; ?><?php if ($icon) : ?><?php print $icon; ?><?php endif; ?><?php if ($title) : ?><h5 class="card-title"><?php print $title; ?></h5><?php endif; ?>
         <?php if ($subtitle) : ?><div class="card-subtitle"><?php print $subtitle; ?></div><?php endif; ?></div><?php endif; ?>
     <?php if ($image) : ?>
-            <div><?php print $image; ?></div>
+            <div class="card-img"><?php print $image; ?></div>
         <?php endif; ?><?php if ($link) : ?></a><?php endif; ?>
     <?php if ($body) : ?>
         <div class="card-body">
